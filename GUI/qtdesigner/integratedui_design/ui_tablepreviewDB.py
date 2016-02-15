e4490baad5cd55a21cab61b9189c5352ccb38471
Andrew Bibian <ajbibian@ucdavis.edu>
2016-02-15 09:35:02 -0600

    These files are the qt and python files that create the class with the dialog widget that displays dataframes in an abstract table view right before it is pushed to the databse. Note, the tables from this abstract table view are editable. The other dialog box (ui_tabledialog.*) is not.

---
+++ GUI/qtdesigner/integratedui_design/ui_tablepreviewDB.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'C:\Users\MillerLab\Dropbox\database-development\GUI\qtdesigner\integratedui_design\ui_tablepreviewDB.ui'
+#
+# Created by: PyQt4 UI code generator 4.11.4
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+class Ui_dialog(object):
+    def setupUi(self, dialog):
+        dialog.setObjectName(_fromUtf8("dialog"))
+        dialog.resize(594, 439)
+        self.verticalLayout_3 = QtGui.QVBoxLayout(dialog)
+        self.verticalLayout_3.setObjectName(_fromUtf8("verticalLayout_3"))
+        self.verticalLayout = QtGui.QVBoxLayout()
+        self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
+        self.horizontalLayout = QtGui.QHBoxLayout()
+        self.horizontalLayout.setSizeConstraint(QtGui.QLayout.SetFixedSize)
+        self.horizontalLayout.setObjectName(_fromUtf8("horizontalLayout"))
+        self.label = QtGui.QLabel(dialog)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Fixed, QtGui.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.label.sizePolicy().hasHeightForWidth())
+        self.label.setSizePolicy(sizePolicy)
+        self.label.setObjectName(_fromUtf8("label"))
+        self.horizontalLayout.addWidget(self.label)
+        spacerItem = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
+        self.horizontalLayout.addItem(spacerItem)
+        self.verticalLayout_2 = QtGui.QVBoxLayout()
+        self.verticalLayout_2.setObjectName(_fromUtf8("verticalLayout_2"))
+        self.btnSitePush = QtGui.QPushButton(dialog)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Fixed, QtGui.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.btnSitePush.sizePolicy().hasHeightForWidth())
+        self.btnSitePush.setSizePolicy(sizePolicy)
+        self.btnSitePush.setObjectName(_fromUtf8("btnSitePush"))
+        self.verticalLayout_2.addWidget(self.btnSitePush)
+        self.horizontalLayout.addLayout(self.verticalLayout_2)
+        self.verticalLayout.addLayout(self.horizontalLayout)
+        self.tblList = QtGui.QTableView(dialog)
+        self.tblList.setObjectName(_fromUtf8("tblList"))
+        self.verticalLayout.addWidget(self.tblList)
+        self.verticalLayout_3.addLayout(self.verticalLayout)
+
+        self.retranslateUi(dialog)
+        QtCore.QMetaObject.connectSlotsByName(dialog)
+
+    def retranslateUi(self, dialog):
+        dialog.setWindowTitle(_translate("dialog", "Preview Data", None))
+        self.label.setText(_translate("dialog", " Does the table below contain all of the correct\n"
+" information? If not adjust values in table.\n"
+" When complete, push to database.", None))
+        self.btnSitePush.setText(_translate("dialog", "Push to DB", None))
+
