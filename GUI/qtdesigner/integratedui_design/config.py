d0400dc61e183954ee398a8bfa3cddb0da7d0225
Andrew Bibian <ajbibian@ucdavis.edu>
2016-02-15 09:26:19 -0600

    This file was created to have the program establish a connection with the postgresql database, instantiate the classes that represent each table within the databse, and provide a Session maker so that we can program interactions with the database in a unit of work pattern

---
+++ GUI/qtdesigner/integratedui_design/config.py
@@ -0,0 +1,58 @@
+# THis file is going to be created to
+# houst the constants of our program i.e.
+# the databse engine and the database session factory maker
+from sqlalchemy import create_engine
+from sqlalchemy import MetaData, Column, Table, ForeignKey
+from sqlalchemy.orm import sessionmaker, aliased
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.automap import automap_base
+import psycopg2
+import datetime as TM
+import logging as log
+
+label = ((str(TM.datetime.now()).split()[0]).replace("-", "_")+".txt")
+
+log.basicConfig(filename = label)
+log.getLogger('sqlalchemy.engine').setLevel(log.INFO)
+
+
+# Creating a connection to the database specifying echo
+# as false so we will not log information twice when
+# writing SQL transactions. THis is at the global level
+# so we can use sessions and unitofwork patterns
+# with sqlalchemy
+#global engine
+engine = create_engine(
+    'postgresql+psycopg2://postgres:demography@localhost/LTER'\
+    , echo= False)
+
+
+
+# Set up the base
+#global base
+base = automap_base()
+
+# And prepare the classes based on the reflection of the
+# database structure
+base.prepare(engine, reflect=True)
+
+# Instantiating the classes which represent our database
+# tables and making their names available at the level
+# of the whole UserInterface main window
+
+ltertable = base.classes.lter
+
+sitetable = base.classes.siteID
+
+maintable = base.classes.main_data
+
+taxatable = base.classes.taxa
+
+rawtable = base.classes.rawobs
+
+
+
+# Creating a factory that can supply connection resources
+# uses sessionmaker
+Session = sessionmaker(bind=engine)
+    
