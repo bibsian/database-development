c77835c4fc97c9dd96675803328e7ce8b71674bb
Andrew Bibian <bibsian@Andrews-Air.att.net>
2016-06-30 10:38:50 -0500

    All components of the program are integrated into this script that makes calls to dialog boxes, initiates the facade class, as well as restarts the program when the users end a session of data munging.

--- test/ui_logic_mainwindow.py
+++ test/ui_logic_mainwindow.py
@@ -1,14 +1,20 @@
-#!usr/bin/env python
+#!/usr/bin/env python
 from PyQt4 import QtGui, QtCore
+from pandas import read_csv
+import subprocess
 import sys, os
 sys.path.append(os.path.realpath(os.path.dirname(__file__)))
 import ui_mainrefactor as mw
 import ui_logic_session as sesslogic
 import ui_logic_site as sitelogic
 import ui_logic_main as mainlogic
+import ui_logic_taxa as taxalogic
+import ui_logic_time as timelogic
+import ui_logic_obs as rawlogic
+import ui_logic_covar as covarlogic
 import class_userfacade as face
 import class_modelviewpandas as view
-
+import class_inputhandler as ini
 
 if sys.platform == 'darwin':
     os.chdir(
@@ -33,15 +39,25 @@ class UiMainWindow(QtGui.QMainWindow, mw.Ui_MainWindow):
         self.dsite = sitelogic.SiteDialog()
         self.dsession = sesslogic.SessionDialog()
         self.dmain = mainlogic.MainDialog()
+        self.dtaxa = taxalogic.TaxaDialog()
+        self.dtime = timelogic.TimeDialog()
+        self.draw = rawlogic.ObsDialog()
+        self.dcovar = covarlogic.CovarDialog()
         
         # Actions
         self.actionSiteTable.triggered.connect(self.site_display)
         self.actionStart_Session.triggered.connect(
             self.session_display)
+        self.actionEnd_Session.triggered.connect(
+            self.end_session)
         self.actionMainTable.triggered.connect(self.main_display)
-
+        self.actionTaxaTable.triggered.connect(self.taxa_display)
+        self.actionTimeFormat.triggered.connect(self.time_display)
+        self.actionRawTable.triggered.connect(self.obs_display)
+        self.actionCovariates.triggered.connect(self.covar_display)
+        self.actionCommit.triggered.connect(self.commit_data)
+        
         # Custom Signals
-        self.dsite.changed_data.connect(self.update_data_model)
         self.dsite.site_unlocks.connect(self.site_complete_enable)
         self.dsession.raw_data_model.connect(
             self.update_data_model)
@@ -52,9 +68,11 @@ class UiMainWindow(QtGui.QMainWindow, mw.Ui_MainWindow):
         self.error = QtGui.QErrorMessage()
         self.message = QtGui.QMessageBox
 
-
-    @QtCore.pyqtSlot(object)
-    def update_data_model(self, dataobject):
+        metadf = read_csv('Datasets_manual_test/meta_file_test.csv')
+        metamodel = view.PandasTableModel(metadf)
+        self.tblViewMeta.setModel(metamodel)
+        
+    def update_data_model(self):
         newdatamodel = view.PandasTableModel(self.facade._data)
         self.tblViewRaw.setModel(newdatamodel)
         self.dsite.facade = self.facade
@@ -62,7 +80,6 @@ class UiMainWindow(QtGui.QMainWindow, mw.Ui_MainWindow):
     @QtCore.pyqtSlot(object)
     def update_webview(self, url):
         self.webView.load(QtCore.QUrl(url))
-
 
     @QtCore.pyqtSlot(object)
     def site_complete_enable(self):
@@ -75,12 +92,12 @@ class UiMainWindow(QtGui.QMainWindow, mw.Ui_MainWindow):
         self.actionTimeFormat.setEnabled(True)
         self.actionRawTable.setEnabled(True)
         self.actionCovariates.setEnabled(True)
+        self.update_data_model()
 
     def site_display(self):
         ''' Displays the Site Dialog box'''
         self.dsite.show()
         self.dsite.facade = self.facade
-
 
     def session_display(self):
         ''' Displays the Site Dialog box'''
@@ -92,3 +109,44 @@ class UiMainWindow(QtGui.QMainWindow, mw.Ui_MainWindow):
         self.dmain.facade = self.facade
         self.dmain.set_data()
         self.dmain.show()
+
+    def taxa_display(self):
+        ''' Display the Taxa Dialog box'''
+        self.dtaxa.facade = self.facade
+        self.dtaxa.show()
+
+    def time_display(self):
+        ''' Display the Time Dialog box'''
+        self.dtime.facade = self.facade
+        self.dtime.show()
+
+    def obs_display(self):
+        ''' Display the Raw Obs Dialog box'''
+        self.draw.facade = self.facade
+        self.draw.show()
+
+    def covar_display(self):
+        '''Display the Raw Obs Dialog box'''
+        self.dcovar.facade = self.facade
+        self.dcovar.show()
+
+    def commit_data(self):
+        commithandle = ini.InputHandler(
+            name='updateinfo', tablename='updatetable')
+        self.facade.input_register(commithandle)
+        try:
+            self.facade.merge_push_data()
+            self.facade.update_main()
+            self.actionCommit.setEnabled(False)
+            self.message.about(
+                self,'Status', 'Database transaction complete')
+        except Exception as e:
+            print(str(e))
+            self.facade._tablelog['maintable'].debug(str(e))
+            self.error.showMessage(
+                'Datbase transaction error: '+ str(e))
+
+    def end_session(self):
+
+        self.close()
+        subprocess.call("python" + " runmain.py", shell=True)
