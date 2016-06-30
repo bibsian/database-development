8366f8b73f1b7a5345ac69dbf4f0c60f39701419
Andrew Bibian <bibsian@Andrews-Air.att.net>
2016-06-30 11:05:53 -0500

    Unit test and logic for the covariable dialog box. Code mostly borrowed from alpha version of the program.

---
+++ test/ui_logic_covar.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+from PyQt4 import QtGui
+import class_inputhandler as ini
+import class_helpers as hlp
+import class_dictionarydataframe as ddf
+import class_modelviewpandas as views
+import ui_dialog_covariate as covar
+import ui_logic_preview as tprev
+
+class CovarDialog(QtGui.QDialog, covar.Ui_Dialog):
+    '''
+    Class to handler the user input into the covariates
+    dialog box i.e. verify inputs and format data.
+    '''
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setupUi(self)
+
+        # Attributes
+        self.covarlned = {'columns': None}
+        self.covarini = None
+        self._log = None
+        self.covardata = None
+        self.covarmodel = None
+
+
+        # Signals/slots
+        self.btnColumns.clicked.connect(self.submit_change)
+        self.btnPreview.clicked.connect(self.submit_change)
+        self.btnSaveClose.clicked.connect(self.submit_change)
+        self.btnCancel.clicked.connect(self.close)
+
+        # Pop up widgets
+        self.message = QtGui.QMessageBox
+        self.error = QtGui.QErrorMessage()
+        self.preview = tprev.TablePreview()
+
+    def submit_change(self):
+        sender = self.sender()
+        self.covarlned['columns'] = hlp.string_to_list(
+            self.lnedColumns.text()
+        )
+
+        self.covarini = ini.InputHandler(
+            name='covarinfo', tablename='covartable',
+            lnedentry=self.covarlned)
+
+        self.facade.input_register(self.covarini)
+        self.facade.create_log_record('covartable')
+        self._log = self.facade._tablelog['covartable']
+
+        try:
+            self.facade._data[self.covarlned['columns']]
+        except Exception as e:
+            print(str(e))
+            self._log.debug(str(e))
+            self.error.showMessage(
+                'Column names not valid: Check spacing ' +
+                'and headers.')
+            raise ValueError('Column names are incorrect')
+
+        try:
+            self.covardata = ddf.DictionaryDataframe(
+                self.facade._data, self.covarlned['columns']
+            ).convert_records()
+        except Exception as e:
+            print(str(e))
+            self._log.debug(str(e))
+            self.error.showMessage('Could not concatenate columns')
+            raise TypeError('Could not concatenate columns')
+
+        self.facade.push_tables['covariates'] = self.covardata
+
+        if sender is self.btnColumns:
+            self.message.about(self, 'Status', 'Columns recorded')
+        elif sender is self.btnPreview:
+            self.covarmodel = views.PandasTableModelEdit(
+                self.covardata)
+            self.preview.tabviewPreview.setModel(self.covarmodel)
+            self.preview.show()
+        elif sender is self.btnSaveClose:
+            hlp.write_column_to_log(
+                self.covarlned, self._log, 'covartable')
+            self.close()
