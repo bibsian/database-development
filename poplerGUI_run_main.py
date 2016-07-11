608df2df8d687629b508808a948ed11a2d0c7076
bibsian <ajbibian@gmail.com>
2016-07-11 13:51:40 -0500

    The main file that will run the program if this repository is cloned and all python dependencies are met (included PyQT4).

---
+++ poplerGUI_run_main.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+from poplerGUI import ui_logic_mainwindow as uv
+from PyQt4 import QtGui
+import sys
+
+def main():
+    
+    app = QtGui.QApplication(sys.argv)
+    ex = uv.UiMainWindow()
+    ex.show()
+    
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
