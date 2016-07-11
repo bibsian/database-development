153438887959a45deae782cb7b4ced9ea5d836e0
bibsian <ajbibian@gmail.com>
2016-07-11 13:24:00 -0500

    Bash file to automatically populate the poplerGUI folder with python modules to run the program.

---
+++ test/update_source.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+cp /Users/bibsian/Dropbox/database-development/test/{__init__.py,class_*,ui*{mainrefactor,session,main,mainwindow,obs,session,site,sitechange,table_preview,preview,taxa,time,obs,covar,dialog*}.py} /Users/bibsian/Dropbox/database-development/poplerGUI/
+echo updated poplgerGUI folder
+cp /Users/bibsian/Dropbox/database-development/test/logiclayer/{class_*,__init__}.py /Users/bibsian/Dropbox/database-development/poplerGUI/logiclayer/
+echo updated logiclayer folder
+cp /Users/bibsian/Dropbox/database-development/test/logiclayer/datalayer/{class_*,__init__,config}.py /Users/bibsian/Dropbox/database-development/poplerGUI/logiclayer/datalayer/
+echo updated datalayer folder
+cp /Users/bibsian/Dropbox/database-development/test/DatabaseConfig/{dbcreationv2.py,LTERV2.png} /Users/bibsian/Dropbox/database-development/db/
+echo database folder
+
