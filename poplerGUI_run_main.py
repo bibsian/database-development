cd225d264fe7f1b7047a25094acc32b567ccf710
bibsian <ajbibian@gmail.com>
2016-07-20 09:55:14 -0500

    Changes subprocess call to correct directory path

--- poplerGUI_run_main.py
+++ poplerGUI_run_main.py
@@ -3,8 +3,8 @@ from poplerGUI import ui_logic_mainwindow as uv
 from PyQt4 import QtGui
 import sys
 
+
 def main():
-    
     app = QtGui.QApplication(sys.argv)
     ex = uv.UiMainWindow()
     ex.show()
