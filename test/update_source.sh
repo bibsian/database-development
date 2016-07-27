0bf92fa8433e3ba36170a580eec715cb667012f4
bibsian <ajbibian@gmail.com>
2016-07-27 15:06:43 -0500

    Updated bash files for coping files across directories, updating python files, and making backups of the database as well as pushing the backup to the cloud.

--- test/update_source.sh
+++ test/update_source.sh
@@ -1,9 +1,9 @@
 #!bin/bash
-cp /Users/bibsian/Dropbox/database-development/test/{__init__.py,class_*,ui*{mainrefactor,session,main,mainwindow,obs,session,site,sitechange,table_preview,preview,taxa,time,obs,covar,dialog*}.py} /Users/bibsian/Dropbox/database-development/poplerGUI/
+cp /Users/bibsian/Desktop/git/database-development/test/{__init__.py,class_*,ui*{mainrefactor,session,main,mainwindow,obs,session,site,sitechange,table_preview,preview,taxa,time,obs,covar,dialog*}.py} /Users/bibsian/Desktop/git/database-development/poplerGUI/
 echo updated poplgerGUI folder
-cp /Users/bibsian/Dropbox/database-development/test/logiclayer/{class_*,__init__}.py /Users/bibsian/Dropbox/database-development/poplerGUI/logiclayer/
+cp /Users/bibsian/Desktop/git/database-development/test/logiclayer/{class_*,__init__}.py /Users/bibsian/Desktop/git/database-development/poplerGUI/logiclayer/
 echo updated logiclayer folder
-cp /Users/bibsian/Dropbox/database-development/test/logiclayer/datalayer/{class_*,__init__,config}.py /Users/bibsian/Dropbox/database-development/poplerGUI/logiclayer/datalayer/
+cp /Users/bibsian/Desktop/git/database-development/test/logiclayer/datalayer/{class_*,__init__,config}.py /Users/bibsian/Desktop/git/database-development/poplerGUI/logiclayer/datalayer/
 echo updated datalayer folder
 
 
