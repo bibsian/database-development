7ef717a7150776e48f08d5eb035c016459f374b0
bibsian <ajbibian@gmail.com>
2016-07-28 09:18:49 -0500

    Updated bash file for backingup database files

--- db/backup.sh
+++ db/backup.sh
@@ -1,5 +1,5 @@
 pg_dump LTERV2 -f ~/Desktop/git/database-development/db/backups/lter_db_backup_$(date +%m-%d-%Y).sql
-pg_dump LTERV2| pg_dump LTERV2 --clean| ssh -C lter@www.how-imodel-it.com "psql popler_test"
+pg_dump LTERV2 | ssh -C lter@www.how-imodel-it.com "psql popler_test"
 
 pg_dump popler__ -f ~/Desktop/git/database-development/db/backups/popler_backup_$(date +%m-%d-%Y).sql
-pg_dump popler__ | pg_dump popler__ --clean | ssh -C lter@www.how-imodel-it.com "psql popler"
+pg_dump popler__ | ssh -C lter@www.how-imodel-it.com "psql popler"
