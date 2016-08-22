827bec87f22d9b2f1f2ba7f2f4f25237c6846cb8
Andrew Bibian <ajbibian@ucdavis.edu>
2016-08-22 12:05:45 -0500

    Removing folder to being construction version 3 of database and program

--- poplerGUI/ui_logic_sitechange.py
+++
@@ -1,7 +0,0 @@
-from PyQt4 import QtGui
-from poplerGUI import ui_dialog_sitechange as uichange
-
-class TablePreview(QtGui.QDialog, uichange.Ui_Dialog):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        self.setupUi(self)
