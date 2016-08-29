09a8c85ac9ca9fa73f2f0d72231a093e78350968
Andrew Bibian <ajbibian@ucdavis.edu>
2016-08-29 17:48:26 -0500

    Test SQL queries to go into unit test and compare pushed data with actual data.

--- test/scratch.py
+++ test/scratch.py
@@ -47,13 +47,21 @@ class individual_table(base):
 
 # First subquery links the site_in_project_table
 # to the study_site table and lter_table
+
 site_in_proj_subq_stmt = (
     select(
-        [study_site_table, site_in_project_table]).
+        [
+            site_in_project_table,
+            study_site_table,
+            lter_table.lat.label('lter_lat'),
+            lter_table.lng.label('lter_lng'),
+            lter_table.lterid,
+            project_table
+        ]).
     select_from(
-        lter_table.__table__.
-        join(study_site_table.__table__.join(site_in_project_table))
-    ).alias('site_in_proj_join')
+        site_in_project_table.__table__.
+        join(study_site_table.__table__).
+        join(lter_table.__table__).join(project_table)).alias()
 )
 
 pretty = pp.PrettyPrinter()
@@ -72,12 +80,10 @@ site_in_proj_subq_df.columns
 # Second subquery links the project_table to our
 # first subquery
 taxa_tbl_subq_stmt = (
-    select([site_in_proj_subq_stmt, project_table, taxa_table]).
+    select([site_in_proj_subq_stmt, taxa_table]).
     select_from(
-        project_table.__table__.
-        join(site_in_proj_subq_stmt.join(taxa_table)
-        )
-    ).alias('taxa join')
+        site_in_proj_subq_stmt.
+        join(taxa_table)).alias()
 )
 
 
@@ -95,39 +101,77 @@ taxa_tbl_subq_df
 
 # Second subquery links the project_table to our
 # first subquery
-count_tbl_subq_stmt = (
-    select([taxa_tbl_subq_stmt, count_table, biomass_table]).
+union_all_select_stmt = union(
+    select([
+        taxa_tbl_subq_stmt,
+        count_table]).
     select_from(
-        project_table.__table__.
+        taxa_tbl_subq_stmt.
         join(
-            taxa_tbl_subq_stmt.join(
-                count_table, onclause=and_(
-                    taxa_tbl_subq_stmt.c.taxa_table_key ==
-                    count_table.taxa_count_fkey,
-                    taxa_tbl_subq_stmt.c.site_in_project_taxa_key ==
-                    count_table.site_in_project_count_fkey
-                )
+            count_table,
+            onclause=and_(
+                taxa_tbl_subq_stmt.c.taxa_table_key ==
+                count_table.taxa_count_fkey,
+                taxa_tbl_subq_stmt.c.site_in_project_key ==
+                count_table.site_in_project_count_fkey
             )
-        ).join(
+        )
+    ).alias('count join'),
+    select([
+        taxa_tbl_subq_stmt,
+        biomass_table]).
+    select_from(
+        taxa_tbl_subq_stmt.
+        join(
             biomass_table,
             onclause=and_(
                 taxa_tbl_subq_stmt.c.taxa_table_key ==
                 biomass_table.taxa_biomass_fkey,
-                taxa_tbl_subq_stmt.c.site_in_project_taxa_key ==
+                taxa_tbl_subq_stmt.c.site_in_project_key ==
                 biomass_table.site_in_project_biomass_fkey
             )
         )
-    ).alias('count join')
+    ).alias('biomass join'),
+    select([
+        taxa_tbl_subq_stmt,
+        density_table]).
+    select_from(
+        taxa_tbl_subq_stmt.
+        join(
+            density_table,
+            onclause=and_(
+                taxa_tbl_subq_stmt.c.taxa_table_key ==
+                density_table.taxa_density_fkey,
+                taxa_tbl_subq_stmt.c.site_in_project_key ==
+                density_table.site_in_project_density_fkey
+            )
+        )
+    ).alias('density join'),
+    select([
+        taxa_tbl_subq_stmt,
+        percent_cover_table]).
+    select_from(
+        taxa_tbl_subq_stmt.
+        join(
+            percent_cover_table,
+            onclause=and_(
+                taxa_tbl_subq_stmt.c.taxa_table_key ==
+                percent_cover_table.taxa_percent_cover_fkey,
+                taxa_tbl_subq_stmt.c.site_in_project_key ==
+                percent_cover_table.site_in_project_percent_cover_fkey
+            )
+        )
+    ).alias('perecent cover join')
 )
 
 
-pretty.pprint(count_tbl_subq_stmt.compile().string)
+pretty.pprint(union_all_select_stmt.compile().string)
 Session = sessionmaker(bind=engine)
 session = Session()
-count_tbl_subq_result = session.execute(count_tbl_subq_stmt)
-count_tbl_subq_df = pd.DataFrame(count_tbl_subq_result.fetchall())
-count_tbl_subq_df.columns = count_tbl_subq_result.keys()
+union_all_select_result = session.execute(union_all_select_stmt)
+union_all_select_df = pd.DataFrame(union_all_select_result.fetchall())
+union_all_select_df.columns = union_all_select_result.keys()
 session.close()
-count_tbl_subq_df
-count_tbl_subq_df.columns
-count_tbl_subq_df.to_csv('count_bio_outerjoin.csv')
+union_all_select_df
+union_all_select_df.columns
+union_all_select_df.to_csv('union_all_select.csv')
