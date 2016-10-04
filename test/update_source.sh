f63ddeac2d73e9ddb27b7cd180106c0f1cedd448
Andrew Bibian <ajbibian@ucdavis.edu>
2016-10-04 14:37:56 -0500

    Deleted old bash file that move files from test directory to poplerGUI directory

--- test/update_source.sh
+++
@@ -1,9 +0,0 @@
-#!bin/bash
-cp /Users/bibsian/Desktop/git/database-development/test/{__init__.py,class_*,ui*{mainrefactor,session,main,mainwindow,obs,session,site,sitechange,table_preview,preview,taxa,time,obs,covar,dialog*}.py} /Users/bibsian/Desktop/git/database-development/poplerGUI/
-echo updated poplgerGUI folder
-cp /Users/bibsian/Desktop/git/database-development/test/logiclayer/{class_*,__init__}.py /Users/bibsian/Desktop/git/database-development/poplerGUI/logiclayer/
-echo updated logiclayer folder
-cp /Users/bibsian/Desktop/git/database-development/test/logiclayer/datalayer/{class_*,__init__,config}.py /Users/bibsian/Desktop/git/database-development/poplerGUI/logiclayer/datalayer/
-echo updated datalayer folder
-
-
