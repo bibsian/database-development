63ceb11ac6268fec6593430da4750bae28c5a6d3
bibsian <ajbibian@gmail.com>
2016-10-17 09:47:19 -0500

    Dialog box to split column from on to many

---
+++ Views/ui_dialog_splitcolumn.py
@@ -0,0 +1,157 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file '/Users/bibsian/Desktop/git/database-development/Views/ui_dialog_splitcolumn.ui'
+#
+# Created by: PyQt4 UI code generator 4.11.4
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName(_fromUtf8("Dialog"))
+        Dialog.resize(471, 163)
+        Dialog.setStyleSheet(_fromUtf8(".QLabel{\n"
+"    background: None;\n"
+"    padding: 0px;\n"
+"    margin: 0px;\n"
+"}\n"
+".QComboBox {\n"
+"    border: 1px solid gray;\n"
+"    border-radius: 7px;\n"
+"    padding: 2px;\n"
+"    padding-left: 15px;\n"
+"    background: #EEEEEE;\n"
+"}\n"
+".QFrame, .QWidget{\n"
+"    border-radius: 7;\n"
+"    background: white;\n"
+"}    \n"
+"\n"
+"\n"
+"\n"
+".QLineEdit{\n"
+"    padding: 1px;\n"
+"    border-style: solid;\n"
+"    border: 1px solid black;\n"
+"    border-radius: 8px;\n"
+"    margin: 0px;\n"
+"}\n"
+"\n"
+".QPushButton {\n"
+"    color: black;\n"
+"    background: #EEEEEE;\n"
+"    border-width: 1px;\n"
+"    border-color: black;\n"
+"    border-style: solid;\n"
+"    border-radius: 7;\n"
+"    margin-top: 0px;\n"
+"    margin-left: 5px;\n"
+"    margin-right:5px;    \n"
+"    padding-left: 5px;\n"
+"    padding-right: 5px;\n"
+"    padding-top: 3px;\n"
+"    padding-bottom: 3px;\n"
+"}\n"
+"\n"
+"\n"
+""))
+        self.verticalLayout_6 = QtGui.QVBoxLayout(Dialog)
+        self.verticalLayout_6.setObjectName(_fromUtf8("verticalLayout_6"))
+        self.verticalLayout_5 = QtGui.QVBoxLayout()
+        self.verticalLayout_5.setSpacing(0)
+        self.verticalLayout_5.setObjectName(_fromUtf8("verticalLayout_5"))
+        self.frame = QtGui.QFrame(Dialog)
+        self.frame.setFrameShape(QtGui.QFrame.StyledPanel)
+        self.frame.setFrameShadow(QtGui.QFrame.Raised)
+        self.frame.setObjectName(_fromUtf8("frame"))
+        self.verticalLayout_4 = QtGui.QVBoxLayout(self.frame)
+        self.verticalLayout_4.setContentsMargins(-1, -1, -1, 12)
+        self.verticalLayout_4.setSpacing(0)
+        self.verticalLayout_4.setObjectName(_fromUtf8("verticalLayout_4"))
+        self.groupBox = QtGui.QGroupBox(self.frame)
+        self.groupBox.setObjectName(_fromUtf8("groupBox"))
+        self.verticalLayout_3 = QtGui.QVBoxLayout(self.groupBox)
+        self.verticalLayout_3.setObjectName(_fromUtf8("verticalLayout_3"))
+        self.gridLayout = QtGui.QGridLayout()
+        self.gridLayout.setObjectName(_fromUtf8("gridLayout"))
+        self.verticalLayout_2 = QtGui.QVBoxLayout()
+        self.verticalLayout_2.setObjectName(_fromUtf8("verticalLayout_2"))
+        self.label = QtGui.QLabel(self.groupBox)
+        self.label.setObjectName(_fromUtf8("label"))
+        self.verticalLayout_2.addWidget(self.label)
+        self.horizontalLayout = QtGui.QHBoxLayout()
+        self.horizontalLayout.setObjectName(_fromUtf8("horizontalLayout"))
+        self.lnedValuecolumns = QtGui.QLineEdit(self.groupBox)
+        self.lnedValuecolumns.setObjectName(_fromUtf8("lnedValuecolumns"))
+        self.horizontalLayout.addWidget(self.lnedValuecolumns)
+        spacerItem = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
+        self.horizontalLayout.addItem(spacerItem)
+        self.verticalLayout_2.addLayout(self.horizontalLayout)
+        self.gridLayout.addLayout(self.verticalLayout_2, 0, 0, 1, 1)
+        self.verticalLayout = QtGui.QVBoxLayout()
+        self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
+        self.label_2 = QtGui.QLabel(self.groupBox)
+        self.label_2.setObjectName(_fromUtf8("label_2"))
+        self.verticalLayout.addWidget(self.label_2)
+        self.horizontalLayout_2 = QtGui.QHBoxLayout()
+        self.horizontalLayout_2.setObjectName(_fromUtf8("horizontalLayout_2"))
+        self.lnedValuecolumns_2 = QtGui.QLineEdit(self.groupBox)
+        self.lnedValuecolumns_2.setObjectName(_fromUtf8("lnedValuecolumns_2"))
+        self.horizontalLayout_2.addWidget(self.lnedValuecolumns_2)
+        spacerItem1 = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
+        self.horizontalLayout_2.addItem(spacerItem1)
+        self.verticalLayout.addLayout(self.horizontalLayout_2)
+        self.gridLayout.addLayout(self.verticalLayout, 0, 1, 1, 1)
+        self.verticalLayout_3.addLayout(self.gridLayout)
+        self.verticalLayout_4.addWidget(self.groupBox)
+        self.verticalLayout_5.addWidget(self.frame)
+        self.horizontalLayout_3 = QtGui.QHBoxLayout()
+        self.horizontalLayout_3.setContentsMargins(5, -1, 5, -1)
+        self.horizontalLayout_3.setObjectName(_fromUtf8("horizontalLayout_3"))
+        self.btnPreview = QtGui.QPushButton(Dialog)
+        self.btnPreview.setFocusPolicy(QtCore.Qt.ClickFocus)
+        self.btnPreview.setObjectName(_fromUtf8("btnPreview"))
+        self.horizontalLayout_3.addWidget(self.btnPreview)
+        spacerItem2 = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
+        self.horizontalLayout_3.addItem(spacerItem2)
+        self.btnSaveClose = QtGui.QPushButton(Dialog)
+        self.btnSaveClose.setFocusPolicy(QtCore.Qt.ClickFocus)
+        self.btnSaveClose.setObjectName(_fromUtf8("btnSaveClose"))
+        self.horizontalLayout_3.addWidget(self.btnSaveClose)
+        spacerItem3 = QtGui.QSpacerItem(5, 20, QtGui.QSizePolicy.Fixed, QtGui.QSizePolicy.Minimum)
+        self.horizontalLayout_3.addItem(spacerItem3)
+        self.btnCancel = QtGui.QPushButton(Dialog)
+        self.btnCancel.setFocusPolicy(QtCore.Qt.ClickFocus)
+        self.btnCancel.setObjectName(_fromUtf8("btnCancel"))
+        self.horizontalLayout_3.addWidget(self.btnCancel)
+        self.verticalLayout_5.addLayout(self.horizontalLayout_3)
+        self.verticalLayout_6.addLayout(self.verticalLayout_5)
+
+        self.retranslateUi(Dialog)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        Dialog.setWindowTitle(_translate("Dialog", "Dialog", None))
+        self.groupBox.setTitle(_translate("Dialog", "Split Column", None))
+        self.label.setText(_translate("Dialog", "Column Name", None))
+        self.label_2.setText(_translate("Dialog", "Split Column On", None))
+        self.btnPreview.setText(_translate("Dialog", "Preview", None))
+        self.btnSaveClose.setText(_translate("Dialog", "Save && Close", None))
+        self.btnCancel.setText(_translate("Dialog", "Cancel", None))
+
