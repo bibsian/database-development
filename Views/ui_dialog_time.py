1fab46d52bcd31fd48a8ace019c27788c3f2cb24
Andrew Bibian <ajbibian@ucdavis.edu>
2016-10-20 15:20:50 -0500

    Updated user views

--- Views/ui_dialog_time.py
+++ Views/ui_dialog_time.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file '/Users/bibsian/Desktop/git/database-development/Views/ui_dialog_time.ui'
+# Form implementation generated from reading ui file 'C:\Users\MillerLab\Desktop\database-development\Views\ui_dialog_time.ui'
 #
 # Created by: PyQt4 UI code generator 4.11.4
 #
