6bb80df32868ba62b5912fc15f1239ad7db530dd
Andrew Bibian <ajbibian@ucdavis.edu>
2016-11-17 13:47:50 -0600

    Changed relationships among tables because of postgres locks when trying to delete records. Also changed the convert_types function for updated pandas syntax (got rid of warning and issue of not saving sometimes)

--- poplerGUI/logiclayer/datalayer/config.py
+++ poplerGUI/logiclayer/datalayer/config.py
@@ -11,6 +11,7 @@ import numpy
 import datetime as dt
 import logging
 import pandas as pd
+import re
 
 # Setup logging for program
 date = (str(dt.datetime.now()).split()[0]).replace("-", "_")
@@ -40,8 +41,14 @@ class study_site_table(base):
     __table__ = Table('study_site_table', metadata, autoload=True)
 class project_table(base):
     __table__ = Table('project_table', metadata, autoload=True)
+    site_in_proj = relationship(
+        'site_in_project_table', cascade="delete, delete-orphan")
+class site_in_project_table(base):
+    __table__ = Table('site_in_project_table', metadata, autoload=True)
     taxa = relationship(
         'taxa_table', cascade="delete, delete-orphan")
+class taxa_table(base):
+    __table__ = Table('taxa_table', metadata, autoload=True)
     count = relationship(
         'count_table', cascade="delete, delete-orphan")
     density = relationship(
@@ -52,10 +59,7 @@ class project_table(base):
         'percent_cover_table', cascade="delete, delete-orphan")
     individual = relationship(
         'individual_table', cascade="delete, delete-orphan")
-class site_in_project_table(base):
-    __table__ = Table('site_in_project_table', metadata, autoload=True)
-class taxa_table(base):
-    __table__ = Table('taxa_table', metadata, autoload=True)
+
 class taxa_accepted_table(base):
     __table__ = Table('taxa_accepted_table', metadata, autoload=True)
 class count_table(base):
@@ -100,25 +104,20 @@ def convert_types(dataframe, types):
     '''
     for i in dataframe.columns:
 
-        #if types[i] in ['FLOAT', 'Float', 'numeric']:
-        #    dataframe.loc[:, i] = pd.to_numeric(dataframe[i], errors='coerce')
-        #if types[i] in ['INTEGER', 'Integer']:
-        #    dataframe.loc[:, i] = dataframe[i].astype(int)
-        if types[i] in ['VARCHAR', 'TEXT', 'VARCHAR(50)', 'VARCHAR(200)']:
+        if types[i] in [
+                'VARCHAR', 'TEXT', 'VARCHAR(50)', 'VARCHAR(200)',
+                'spatial_replication_level_1', 'spatial_replication_level_2',
+                'spatial_replication_level_3', 'spatial_replication_level_4',
+                'spatial_replication_level_5']:
             try:
-                dataframe.loc[:, i] = dataframe[i].astype(str)
+                dataframe[i] = dataframe[i].apply(str).values
                 print('In CONVERT: ', i ,dataframe.loc[:,i].dtypes)
             except Exception as e:
                 print('string conversion did not work:', i, str(e))
-            
-            dataframe.loc[:, i] = dataframe[i].astype(object)
-        if i in ['year', 'month', 'day']:
-            dataframe.loc[:, i] = pd.to_numeric(dataframe[i], errors='coerce')
-            dataframe[i].fillna('NaN', inplace=True)
-        if 'observation' in i or 'extent' in i or 'unique_rep' in i:
-            dataframe.loc[:, i] = pd.to_numeric(dataframe[i], errors='coerce')
+            dataframe[i] = dataframe[i].astype(object).values
 
-        print('CONVERSION TYPES: ', i, types[i])
+        if re.search('observation', i) is not None or re.search('_extent$', i) is not None or re.search('unique_reps', i) is not None:
+            dataframe[i] = pd.to_numeric(dataframe[i], errors='coerce').values
 
 
 
